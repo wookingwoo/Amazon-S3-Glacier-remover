6cf4388ac8511750886d20c527d6366bcf6803c6
JeongHeon Woo <email@wookingwoo.com>
2023-06-15 03:05:15 +0900

    Add tqdm

--- delete_all_archives.py
+++ delete_all_archives.py
@@ -1,23 +1,31 @@
 import boto3
+from tqdm import tqdm
+
+import parsing_archiveid
 from data import data
 
 
 def delete_all_archives(vault_name, region_name, archive_ids):
     client = boto3.client('glacier', region_name=region_name)
 
-    for archive_id in archive_ids:
-        try:
-            response = client.delete_archive(
-                vaultName=vault_name,
-                archiveId=archive_id
-            )
-            print(f"Deleted archive {archive_id}")
-        except Exception as e:
-            print(f"Failed to delete archive {archive_id}. Reason: {str(e)}")
+    with tqdm(total=len(archive_ids), desc="Deleting archives") as pbar:
+        for archive_id in archive_ids:
+            try:
+                response = client.delete_archive(
+                    vaultName=vault_name,
+                    archiveId=archive_id
+                )
+                pbar.set_postfix({"Deleted": archive_id})
+            except Exception as e:
+                pbar.set_postfix({"Failed": archive_id})
+                pbar.write(f"Failed to delete archive {archive_id}. Reason: {str(e)}")
+            finally:
+                pbar.update(1)
 
 
 # List of your archive ids. You need to fill this.
-archive_ids = data.archive_ids1  # 예: ["your_archive_id_1", "your_archive_id_2", "your_archive_id_3"]
+archive_ids = parsing_archiveid.get_archive_id_list(
+    './data/output_json/output1.json')  # 예: ["your_archive_id_1", "your_archive_id_2", "your_archive_id_3"]
 
 vault_name = data.vault_name1
 region_name = data.region_name  # 예: 'us-west-2'
